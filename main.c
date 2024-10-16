9d4ef2591153e8023078f9348e5e646681b7f29c
Fayez-Hossain <shahadat20055@gmail.com>
2024-10-16 21:46:10 +0600

    ASCII-value-of-char

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    char num;
+
+    printf("enter your character to view ASCII value:");
+    scanf("%c",&num);
+
+    printf("ASCII value is:%d",num);
+
+    return 0;
+
+}
